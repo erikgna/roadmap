7c7f73bf90b60e7d7341cc4d8ba79d14cbebbf8d
erikgna <erikgnaa@gmail.com>
2021-03-30 21:57:16 -0300

    front_end

---
+++ MERN/mern-road-map/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class EditExercise extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Edit List page</h1>
+            </div>
+        )
+    }
+}
